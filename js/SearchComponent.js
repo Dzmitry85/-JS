a6f5446dd60bbb6f595f1a288dec2c08f6e14b2e
Dzmitry85 <dpinchuk85@gmail.com>
2021-06-17 20:41:46 +0300

    Практическое задание-7

--- js/SearchComponent.js
+++
@@ -1,8 +0,0 @@
-Vue.component('searchform', {   //searchform - название тега в ДОМ
-    props: ['value'],
-    template: `
-        <input type="text" class="search-field" 
-        v-bind:value="value"
-        v-on:input="$emit('input', $event.target.value)">
-    `
-})
